433747e5126f0281ac3eb262bde4a2f8da067e9d
Jaeuk Oh <wooko5@naver.com>
2024-06-25 15:38:56 +0900

    init project

---
+++ src/com/kotlin_basic/Main.kt
@@ -0,0 +1,13 @@
+package com.kotlin_basic
+
+fun main() {
+    //print문
+    println("Hello World!")
+
+    //변수 선언
+    var i : Int = 10
+    var name : String = "jaeuk"
+    var point : Double = 3.3
+
+    println()
+}
