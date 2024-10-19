fcb798a58fc386f70d76562958ca4531e9b6373d
Jaeuk Oh <wooko5@naver.com>
2024-10-19 18:17:49 +0900

    feat : data class를 이용한 롬복제거

---
+++ src/com/kotlin/java_to_kotlin/part1/_05/Hero.java
@@ -0,0 +1,8 @@
+package com.kotlin.java_to_kotlin.part1._05;
+
+
+public class Hero {
+    private String name;
+    private int age;
+    private String address;
+}
---
+++ src/com/kotlin/java_to_kotlin/part1/_05/Hero.kt
@@ -0,0 +1,7 @@
+package com.kotlin.java_to_kotlin.part1._05
+
+data class Hero(
+    val name: String,
+    val age: Int,
+    val address: String
+)
